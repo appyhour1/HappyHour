f1ef495bcc31515c341973c2d777b0f6284f6fe5
appyhour1 <appyhour1@gmail.com>
2026-04-27 16:37:37 -0400

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
-import { AppProvider } from './contexts/AppContext'
-import AppShell from './AppShell'
-import BrowsePage from './pages/BrowsePage'
-import VenueDetailPage from './pages/VenueDetailPage'
-import SeoLandingPage from './pages/SeoLandingPage'
-import CrawlBuilderPage from './pages/CrawlBuilderPage'
-import NowPage from './pages/NowPage'
-import TonightPage from './pages/TonightPage'
-import EmailPreviewPage from './pages/EmailPreviewPage'
-import PrivacyPage from './pages/PrivacyPage'
-import TermsPage from './pages/TermsPage'
-import AboutPage from './pages/AboutPage'
-import AdminPage from './pages/AdminPage'
-import './styles.css'
+import './styles/mobile-fix.css'
 import './styles/additions.css'
+import './styles.css'
+import App from './App'
 
-// Register service worker for PWA
+// Unregister any existing service workers
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').catch(() => {})
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister())
   })
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+)
+
 root.render(
-  <HelmetProvider>
-    <AppProvider>
-      <BrowserRouter>
-        <AppShell>
-          <Routes>
-            <Route path="/" element={<BrowsePage />} />
-            <Route path="/venue/:id" element={<VenueDetailPage />} />
-            <Route path="/crawl" element={<CrawlBuilderPage />} />
-            <Route path="/now" element={<NowPage />} />
-            <Route path="/tonight" element={<TonightPage />} />
-            <Route path="/admin/email-preview" element={<EmailPreviewPage />} />
-            <Route path="/privacy" element={<PrivacyPage />} />
-            <Route path="/terms" element={<TermsPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/admin" element={<AdminPage />} />
-            <Route path="/:city" element={<SeoLandingPage />} />
-            <Route path="/:city/:slug" element={<SeoLandingPage />} />
-          </Routes>
-        </AppShell>
-      </BrowserRouter>
-    </AppProvider>
-  </HelmetProvider>
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
 )
